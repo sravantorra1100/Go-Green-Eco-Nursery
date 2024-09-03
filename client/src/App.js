887b3249da3698f6bd92dc8cb9526d49e0573167
sravantorra1100 <sravantorra1100@gmail.com>
2024-09-03 11:53:47 +0530

    3/9 frontend WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Pagenotfound from './pages/Pagenotfound';
+import Policy from './pages/Policy';
 
 function App() {
   return (
     <>
-    sravan
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='*' element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
