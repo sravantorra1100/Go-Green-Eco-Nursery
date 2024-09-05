86af2274482bc7de64569d998fef3097154c4e8b
sravantorra1100 <sravantorra1100@gmail.com>
2024-09-05 21:57:28 +0530

    05/09 WIP working on frontend

--- client/src/pages/Admin/CreateProduct.js
+++ client/src/pages/Admin/CreateProduct.js
@@ -1,20 +1,98 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from '../../components/Layout/Layout'
 import AdminMenu from '../../components/Layout/AdminMenu'
+import toast from 'react-hot-toast';
+import axios from 'axios';
+import { Select } from 'antd';
+const { Option } = Select;
 
 const CreateProduct = () => {
+  const [categories, setCategories] = useState([])
+  const [category, setCategory] = useState("")
+  const [name, setName] = useState("")
+  const [description, setDescription] = useState("")
+  const [price, setPrice] = useState("")
+  const [quantity, setQuantity] = useState("")
+  const [shipping, setShipping] = useState("")
+  const [photo, setPhoto] = useState("")
+
+  //get all category
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get('/api/v1/category/get-category')
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error)
+      toast.error('something went wrong in getting categories')
+    }
+  }
+
+  useEffect(() => {
+    getAllCategory();
+  }, [])
+
+//handle create - product
+const handleCreate=()=>{ }
+
   return (
     <Layout title={'Dashboard - Create Product'}>
-         <div className="container-fluid m-3 p-3">
-    <div className="row">
-            <div className="col-md-3">
-                <AdminMenu/>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1>Create Product</h1>
+            <div className="m-1 w-75">
+              <Select bordered={false} placeholder="Select a category" size="large" showSearch
+                className='form-select mb-3' onChange={(value) => { setCategory(value) }}>
+                {categories?.map(c => (
+                  <Option key={c._id} value={c.name}>{c.name}</Option>
+                ))}
+              </Select>
+              <div className="mb-3">
+                <label className='btn btn-outline-secondary col-md-12'>
+                  {photo ? photo.name : "Upload photo"}
+                  <input type="file" name='photo' accept='image/*' onChange={(e) => setPhoto(e.target.files[0])} hidden />
+                </label>
+              </div>
+              <div className="mb-3">
+                {photo && (
+                  <div className="text-center">
+                    <img src={URL.createObjectURL(photo)} alt="product_photo" height={'200px'} className='img img-responsive' />
+                  </div>
+                )}
+              </div>
+              <div className="mb-3">
+                <input type="text" value={name} placeholder='write a name' className='form-control' onChange={(e) => setName(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <textarea type="text" value={description} placeholder='write a description' className='form-control' onChange={(e) => setDescription(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <input type="number" value={price} placeholder='write a price' className='form-control' onChange={(e) => setPrice(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <input type="number" value={quantity} placeholder='write a quantity' className='form-control' onChange={(e) => setQuantity(e.target.value)} />
+              </div>
+              <div className="mb-3">
+                <Select bordered={false} placeholder="select shipping" size='large' showSearch 
+                className='form-select mb-3' onChange={(value)=>{setShipping(value);}}>
+                   <option value="0">No</option>
+                   <option value="1">Yes</option>
+                </Select>
+              </div>
+               <div className="mb-3">
+                <button className='btn btn-primary' onClick={handleCreate}>
+                  CREATE PRODUCT
+                </button>
+               </div>
             </div>
-            <div className="col-md-9">
-                <h1>Create Product</h1>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </Layout>
   )
 }
